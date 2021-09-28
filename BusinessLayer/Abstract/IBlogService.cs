fe4ddae6dc813b3254c0adcbf27d66fc4f0e2850
mertaksu28 <mertaksu28@outlook.com.tr>
2021-09-28 23:25:31 +0300

    Blogs Lists and Theme Edit

---
+++ BusinessLayer/Abstract/IBlogService.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IBlogService
+    {
+        List<Blog> GetAll();
+        Blog GetById(int id);
+        void Add(Blog blog);
+        void Delete(Blog blog);
+        void Update(Blog blog);
+    }
+}
---
+++ BusinessLayer/Concrete/BlogManager.cs
@@ -0,0 +1,47 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class BlogManager : IBlogService
+    {
+
+        IBlogDal _blogDal;
+
+        public BlogManager(IBlogDal blogDal)
+        {
+            _blogDal = blogDal;
+        }
+
+        public void Add(Blog blog)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Blog blog)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Blog> GetAll()
+        {
+            return _blogDal.GetAll();
+        }
+
+        public Blog GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Blog blog)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
