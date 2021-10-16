3ff1a3ef67f54a89e1978a08a831a8844dc6ec96
mertaksu28 <mertaksu28@outlook.com.tr>
2021-10-16 23:08:46 +0300

    CitiesForRegisterPage

--- CoreDemo/Controllers/RegisterController.cs
+++ CoreDemo/Controllers/RegisterController.cs
@@ -1,9 +1,14 @@
 ﻿using BusinessLayer.Concrete;
 using BusinessLayer.ValidationRules;
+using CoreDemo.Models;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
 using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CoreDemo.Controllers
 {
@@ -14,6 +19,7 @@ namespace CoreDemo.Controllers
         [HttpGet]
         public IActionResult Index()
         {
+            ViewBag.Cities = GetCityList();
             return View();
         }
 
@@ -41,6 +47,21 @@ namespace CoreDemo.Controllers
                 ModelState.AddModelError("WriterPassword", "Şifreler Eşleşmedi Lütfen Tekrar Deneyin!");
             }
             return View();
+        }
+        public List<SelectListItem> GetCityList()
+        {
+            List<SelectListItem> adminRole = (from x in GetCity()
+                                              select new SelectListItem
+                                              {
+                                                  Text = x,
+                                                  Value = x
+                                              }).ToList();
+            return adminRole;
+        }
+        public List<string> GetCity()
+        {
+            String[] citiesArray = new String[] { "Adana", "Adıyaman", "Afyonkarahisar", "Ağrı", "Aksaray", "Amasya", "Ankara", "Antalya", "Ardahan", "Artvin", "Aydın", "Balıkesir", "Bartın", "Batman", "Bayburt", "Bilecik", "Bingöl", "Bitlis", "Bolu", "Burdur", "Bursa", "Çanakkale", "Çankırı", "Çorum", "Denizli", "Diyarbakır", "Düzce", "Edirne", "Elazığ", "Erzincan", "Erzurum", "Eskişehir", "Gaziantep", "Giresun", "Gümüşhane", "Hakkâri", "Hatay", "Iğdır", "Isparta", "İstanbul", "İzmir", "Kahramanmaraş", "Karabük", "Karaman", "Kars", "Kastamonu", "Kayseri", "Kilis", "Kırıkkale", "Kırklareli", "Kırşehir", "Kocaeli", "Konya", "Kütahya", "Malatya", "Manisa", "Mardin", "Mersin", "Muğla", "Muş", "Nevşehir", "Niğde", "Ordu", "Osmaniye", "Rize", "Sakarya", "Samsun", "Şanlıurfa", "Siirt", "Sinop", "Sivas", "Şırnak", "Tekirdağ", "Tokat", "Trabzon", "Tunceli", "Uşak", "Van", "Yalova", "Yozgat", "Zonguldak" };
+            return new List<string>(citiesArray);
         }
     }
 }
