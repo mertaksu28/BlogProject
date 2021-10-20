03abe17f4c3d78d7415db34cae76e012748896be
mertaksu28 <mertaksu28@outlook.com.tr>
2021-10-20 17:00:57 +0300

    View Components and Writer Navbar Edit

--- CoreDemo/Controllers/WriterController.cs
+++ CoreDemo/Controllers/WriterController.cs
@@ -31,5 +31,16 @@ namespace CoreDemo.Controllers
             return View();
         }
 
+        [AllowAnonymous]
+        public PartialViewResult WriterNavbarPartial()
+        {
+            return PartialView();
+        }
+        [AllowAnonymous]
+        public PartialViewResult WriterFooterPartial()
+        {
+            return PartialView();
+        }
+
     }
 }
