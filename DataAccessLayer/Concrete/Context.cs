16fcf905e89c0032620425685552d582387a9ff2
mertaksu28 <mertaksu28@outlook.com.tr>
2021-11-16 13:17:35 +0300

    Notification Class Added

--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -23,5 +23,6 @@ namespace DataAccessLayer.Concrete
         public DbSet<Writer> Writers { get; set; }
         public DbSet<NewsLetter> NewsLetters { get; set; }
         public DbSet<BlogRayting> BlogRaytings { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
     }
 }
---
+++ DataAccessLayer/Migrations/20211116095812_mig_table_notification_add.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class mig_table_notification_add : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Notifications",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NotificationType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NotificationTypeSymbol = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NotificationDetails = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NotificationStatus = table.Column<bool>(type: "bit", nullable: false),
+                    NotificationDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notifications", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Notifications");
+        }
+    }
+}
